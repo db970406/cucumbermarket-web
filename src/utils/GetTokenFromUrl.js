9da4e1595538730f91efc2193ab0443009586d84
최성준 <db970406@naver.com>
2022-01-16 19:30:39 +0900

    소셜로그인 code추출 후 서버에 fetch보내는 기능 함수화

--- src/utils/GetTokenFromUrl.js
+++ src/utils/GetTokenFromUrl.js
@@ -9,59 +9,38 @@
 import { useLocation, useParams } from 'react-router-dom';
 import { logUserIn } from './apollo';
 
+// code를 추출하고 서버에 fetch하는 함수
+const extractCodeAndFetchToServer = async (social, location) => {
+    const code = location.search.split('=')[1].split('&')[0];
+    const response = await fetch(`http://localhost:4000/social/${social}`, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({ code })
+    })
+    if (response.status === 200) {
+        const { jwtToken } = await response.json()
+        console.log(jwtToken)
+        return logUserIn(jwtToken)
+    }
+}
+
 const GetTokenFromUrl = async () => {
     const { social } = useParams()
     const location = useLocation();
 
     const processLogin = async () => {
+        // 각 소셜로그인 Component로 해당 사이트에 요청을 보낸 후 받은 token을 추출하여 백엔드에 전달하여 처리하고 jwtToken을 json데이터로 돌려받아 localStorage에 저장
         switch (social) {
-            // NaverLogin으로 네이버에 요청을 보낸 후 받은 token을 추출하여 백엔드에 전달하여 처리하고 jwtToken을 json데이터로 돌려받아 localStorage에 저장
             case "naver":
-                const naverCode = location.search.split('=')[1].split('&')[0];
-                const naverLoginResponse = await fetch(`http://localhost:4000/social/naver`, {
-                    method: "POST",
-                    headers: {
-                        "Content-type": "application/json"
-                    },
-                    body: JSON.stringify({ naverCode })
-                })
-                if (naverLoginResponse.status === 200) {
-                    const { jwtToken } = await naverLoginResponse.json()
-                    logUserIn(jwtToken)
-                }
-                break;
+                return extractCodeAndFetchToServer("naver", location)
 
-            // GithubLogin으로 깃허브에 요청을 보낸 후 받은 code를 추출하여 백엔드에 전달하여 처리하고 json데이터로 돌려받아 localStorage에 저장
             case "github":
-                const githubCode = location.search.split('=')[1].split('&')[0];
-                const githubLoginResponse = await fetch(`http://localhost:4000/social/github`, {
-                    method: "POST",
-                    headers: {
-                        "Content-type": "application/json"
-                    },
-                    body: JSON.stringify({ githubCode })
-                })
-
-                if (githubLoginResponse.status === 200) {
-                    const { jwtToken } = await githubLoginResponse.json()
-                    logUserIn(jwtToken)
-                }
-                break;
+                return extractCodeAndFetchToServer("github", location)
 
             case "kakao":
-                const kakaoCode = location.search.split('=')[1].split('&')[0];
-                const kakaoLoginResponse = await fetch(`http://localhost:4000/social/kakao`, {
-                    method: "POST",
-                    headers: {
-                        "Content-type": "application/json"
-                    },
-                    body: JSON.stringify({ kakaoCode })
-                })
-
-                if (kakaoLoginResponse.status = 200) {
-                    const { jwtToken } = await kakaoLoginResponse.json()
-                    logUserIn(jwtToken)
-                }
+                return extractCodeAndFetchToServer("kakao", location)
         }
     }
     processLogin()
