63147cf8874423282ce4ea9bbbd0dd62a0eea5a9
최성준 <db970406@naver.com>
2022-01-18 14:56:04 +0900

    Fix: 변경 ToggleLike 동작 후 cache 관련 버그 수정
    ToggleLike를 해제했다가 다시 클릭했을 때 Item이 여러 개로 번지는 cache 버그 수정

--- src/components/main/items/ToggleLike.js
+++ src/components/main/items/ToggleLike.js
@@ -61,16 +61,11 @@ export default function ToggleLike({ itemId, isLiked }) {
                         return isLiked ? prev - 1 : prev + 1
                     },
                     likes(prev) {
-                        /*
-                        관심을 누른 상태라면 존재하는 likes 배열에서 write한 fragment와 같은 값을 배열에서 제거한다.
+                        //관심을 누른 상태라면 존재하는 likes 배열에서 write한 fragment와 같은 값을 배열에서 제거한다.
                         const clearMyLikes = prev.filter(item => item.__ref !== photoPressedLike.__ref)
 
-                        관심을 누르지 않았다면 write한 fragment를 likes 배열에 추가한다.
-                        return isLiked ? clearMyLikes : [photoPressedLike, ...prev]
-                        */
-
-                        // 유저 사용성을 고려했을 때 관심 해제 시 UserDetail에서 해당 Item을 바로 사라지게 하는 것보단 Item은 두되 관심만 해제시키는게 나은 듯?
-                        return isLiked ? prev : [photoPressedLike, ...prev]
+                        //관심을 누르지 않았다면 그대로 이전 배열을 사용한다.
+                        return isLiked ? clearMyLikes : [...prev]
                     }
                 }
             })
